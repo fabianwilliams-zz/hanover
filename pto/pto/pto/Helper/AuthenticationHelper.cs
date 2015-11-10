b3f6ed49bf357b16584d25b2d74d0bf4e2f719b3
Fabian Williams <fabian@adotob.com>
2015-11-10 13:09:59 -0500

    Configure for Xamarin on Mac

---
+++ pto/pto/pto/Helper/AuthenticationHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace pto
+{
+	public class AuthenticationHelper
+	{
+		public const string Authority = "https://login.windows.net/common";
+		public static Uri returnUri = new Uri("http://");
+		public static string clientId = "";
+		public static AuthenticationContext authContext = null;
+		public static string SharePointURL = "https://";
+
+
+		public static async Task<AuthenticationResult> GetAccessToken(string serviceResourceId, PlatformParameters param)
+		{
+			authContext = new AuthenticationContext(Authority);
+			if (authContext.TokenCache.ReadItems ().Any ())
+				authContext = new AuthenticationContext (authContext.TokenCache.ReadItems ().First ().Authority);
+			var authResult = await authContext.AcquireTokenAsync(serviceResourceId, clientId, returnUri, param);
+			return authResult;
+		}
+	}
+}
+
