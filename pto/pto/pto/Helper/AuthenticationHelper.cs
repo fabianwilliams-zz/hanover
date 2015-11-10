923858a3653711f98a158db0b933b05c9151c8b6
Fabian Williams <fabian@adotob.com>
2015-11-10 13:39:40 -0500

    Added ADAL preview reference to all projects

--- pto/pto/pto/Helper/AuthenticationHelper.cs
+++ pto/pto/pto/Helper/AuthenticationHelper.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace pto
 {
 	public class AuthenticationHelper
 	{
-		public const string Authority = "https://login.windows.net/common";
-		public static Uri returnUri = new Uri("http://");
-		public static string clientId = "";
+
+        public const string Authority = "https://login.windows.net/common";
+		public static Uri returnUri = new Uri("http://speakerfabian.azurewebsites.net");
+		public static string clientId = "35f35e5f-0a4a-4bde-8362-2ea0fc01abb4";
 		public static AuthenticationContext authContext = null;
-		public static string SharePointURL = "https://";
+		public static string SharePointURL = "https://fabiansworld.sharepoint.com";
 
 
 		public static async Task<AuthenticationResult> GetAccessToken(string serviceResourceId, PlatformParameters param)
